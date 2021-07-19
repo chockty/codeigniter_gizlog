d688934ea57bdfee0aed823af303932d3e32394f
chockty <75976150+chockty@users.noreply.github.com>
2021-07-19 12:40:12 +0900

    マイグレーション実行用のコントローラを作成

---
+++ application/controllers/Migrate.php
@@ -0,0 +1,38 @@
+<?php
+
+class Migrate extends CI_Controller
+{
+    public function __construct()
+		{
+			  parent::__construct();
+			  $this->load->library('migration');
+		}
+
+    public function current()
+    {
+        if ($this->migration->current()) {
+					  log_message('success', 'Migration Success.');
+        } else {
+					  show_error($this->migration->error_string());
+				}
+    }
+
+		public function rollback($version)
+    {
+        if ($this->migration->version($version)) {
+					  log_message('success', 'Migration Success.');
+        } else {
+					  show_error($this->migration->error_string());
+				}
+    }
+
+		public function lastest()
+    {
+        if ($this->migration->current()) {
+					  log_message('success', 'Migration Success.');
+        } else {
+					  show_error($this->migration->error_string());
+				}
+    }
+
+}
