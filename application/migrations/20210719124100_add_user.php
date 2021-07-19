32d5646e2c52a2f4afb1d37e4cc597d71759607b
chockty <75976150+chockty@users.noreply.github.com>
2021-07-19 13:20:57 +0900

    userテーブル用のマイグレーションファイルを作成

---
+++ application/migrations/20210719124100_add_user.php
@@ -0,0 +1,57 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_blog extends CI_Migration {
+
+        public function up()
+        {
+                $this->dbforge->add_field(array(
+                        'id' => array(
+                                'type' => 'INT',
+                                'unsigned' => TRUE,
+                                'auto_increment' => TRUE
+                        ),
+                        'name' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '255',
+                        ),
+                        'email' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '255',
+                        ),
+                        'avator' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '255',
+																'null' => TRUE,
+                        ),
+												'password' => array(
+												        'type' => 'CHAR',
+																'constraint' => '100',
+												),
+                        'remember_token' => array(
+													      'type' => 'VARCHAR',
+													      'constraint' => '255',
+												),
+												'created_at' => array(
+                                'type' => 'TIMESTAMP',
+                                'null' => TRUE,
+                        ),
+												'updated_at' => array(
+                                'type' => 'TIMESTAMP',
+                                'null' => TRUE,
+                        ),
+												'updated_at' => array(
+                                'type' => 'TIMESTAMP',
+                                'null' => TRUE,
+                        )
+                ));
+                $this->dbforge->add_key('blog_id', TRUE);
+                $this->dbforge->create_table('daily_report');
+        }
+
+        public function down()
+        {
+                $this->dbforge->drop_table('daily_report');
+        }
+}
