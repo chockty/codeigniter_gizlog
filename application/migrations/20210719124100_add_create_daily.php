75c2892041abbdd5c3242fd964d3c80f93a31aa5
chockty <75976150+chockty@users.noreply.github.com>
2021-07-19 13:06:50 +0900

    daily_reportテーブル用のマイグレーションファイルを作成

---
+++ application/migrations/20210719124100_add_create_daily.php
@@ -0,0 +1,50 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_blog extends CI_Migration {
+
+        public function up()
+        {
+                $this->dbforge->add_field(array(
+                        'id' => array(
+                                'type' => 'INT',
+                                'unsigned' => TRUE,
+                                'auto_increment' => TRUE
+                        ),
+                        'user_id' => array(
+                                'type' => 'INT',
+                                'constraint' => '100',
+                        ),
+                        'title' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '255',
+                        ),
+												'content' => array(
+                                'type' => 'TEXT',
+                        ),
+												'reporting_time' => array(
+                                'type' => 'DATETIME',
+                        ),
+												'created_at' => array(
+                                'type' => 'TIMESTAMP',
+                                'null' => TRUE,
+                        ),
+												'updated_at' => array(
+                                'type' => 'TIMESTAMP',
+                                'null' => TRUE,
+                        ),
+												'updated_at' => array(
+                                'type' => 'TIMESTAMP',
+                                'null' => TRUE,
+                        )
+                ));
+                $this->dbforge->add_key('blog_id', TRUE);
+                $this->dbforge->create_table('daily_report');
+        }
+
+        public function down()
+        {
+                $this->dbforge->drop_table('daily_report');
+        }
+}
