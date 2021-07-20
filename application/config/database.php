b07f9d18addae773e3ab39add7e88162aadfec1b
chockty <75976150+chockty@users.noreply.github.com>
2021-07-20 16:53:34 +0900

    モデル用のdb接続設定を追加

--- application/config/database.php
+++ application/config/database.php
@@ -91,6 +91,26 @@ $db['default'] = array(
 	'encrypt' => FALSE,
 	'compress' => FALSE,
 	'stricton' => FALSE,
-	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['model'] = array(
+	'dsn'	=> '',
+	'hostname' => 'db',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'gizlog',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => TRUE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
 	'save_queries' => TRUE
 );
