2716d0d264f7ae66d0a720b47493ac3f66201bab
chockty <75976150+chockty@users.noreply.github.com>
2021-07-21 13:05:58 +0900

    日報作成画面を調整

--- application/views/user/create.php
+++ application/views/user/create.php
@@ -4,7 +4,6 @@
 		<?php echo form_open('report'); ?>
       <div class="form-group form-size-small <? if (form_error('reporting_time')) : ?> has-error <? endif; ?>">
 				<input type="date" name="reporting_time" class="form-control" value="<? echo set_value('reporting_time') ?>">
-        <!-- {!! Form::date('reporting_time', date('Y-m-d'), ['class' => 'form-control']) !!} -->
         <? if (form_error('reporting_time')) : ?>
           <span class="help-block"><?php echo form_error('reporting_time'); ?></span>
 				<? endif; ?>
@@ -14,23 +13,13 @@
 				<? if (form_error('title')) : ?>
           <span class="help-block"><?php echo form_error('title'); ?></span>
 				<? endif; ?>
-        <!-- {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Title']) !!} -->
-        <!-- @if ($errors->has('title'))
-          <span class="help-block">{{ $errors->first('title') }}</span>
-        @endif -->
       </div>
       <div class="form-group <? if (form_error('content')) : ?> has-error <? endif; ?>">
 				<textarea name="content" placeholder="Content" class="form-control" cols="50" rows="10"><? echo set_value('content') ?></textarea>
 				<? if (form_error('content')) : ?>
           <span class="help-block"><?php echo form_error('content'); ?></span>
 				<? endif; ?>
-        <!-- {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Content', 'cols' => '50', 'rows' => '10']) !!}
-        @if ($errors->has('content'))
-          <span class="help-block">{{ $errors->first('content') }}</span>
-        @endif -->
       </div>
-			<input type="submit" value="submit" class="btn btn-success pull-right">
-      <!-- {!! Form::button('Add', ['type' => 'submit', 'class' => 'btn btn-success pull-right']) !!} -->
-    <!-- {!! Form::close() !!} -->
+			<input type="submit" name="submit" value="Add" class="btn btn-success pull-right">
   </div>
 </div>
