648136d3efc41a5cb454fb5288e2c44fb446c313
chockty <75976150+chockty@users.noreply.github.com>
2021-07-21 17:11:29 +0900

    日報詳細ページを実装

--- application/views/user/daily_report/show.php
+++ application/views/user/daily_report/show.php
@@ -2,18 +2,18 @@
 <div class="main-wrap">
   <div class="panel panel-success">
     <div class="panel-heading">
-      {{ $dailyReport->reporting_time->format('Y/m/d (D)') }}の日報
+			<? echo html_escape(nice_date($daily_report['reporting_time'], 'Y-m-d')) ?>&nbsp;の日報
     </div>
     <div class="table-responsive">
       <table class="table table-striped table-bordered">
         <tbody>
           <tr>
             <th class="table-column">Title</th>
-            <td class="td-text">{{ $dailyReport->title }}</td>
+            <td class="td-text"><? echo html_escape(nl2br_except_pre($daily_report['title'])) ?></td>
           </tr>
           <tr>
             <th class="table-column">Content</th>
-            <td class='td-text'>{!! nl2br(e($dailyReport->content)) !!}</td>
+            <td class='td-text'><? echo html_escape($daily_report['content']) ?></td>
           </tr>
         </tbody>
       </table>
@@ -22,9 +22,10 @@
   <div class="btn-bottom-wrapper">
     <a class="btn btn-edit" href="{{ route('report.edit', $dailyReport->id) }}"><i class="fa fa-pencil" aria-hidden="true"></i></a>
     <div class="btn-delete">
-      {!! Form::open(['route' => ['report.destroy', $dailyReport->id], 'method' => 'DELETE']) !!}
+			<!-- 削除機能は後ほど -->
+      <!-- {!! Form::open(['route' => ['report.destroy', $dailyReport->id], 'method' => 'DELETE']) !!}
         {!! Form::button('<i class="fa fa-trash-o"></i>', ['type' => 'submit', 'class' => 'btn btn-danger']) !!}
-      {!! Form::close() !!}
+      {!! Form::close() !!} -->
     </div>
   </div>
 </div>
