ef6b549be4a452894c2df6330d01002875a0fa6a
chockty <75976150+chockty@users.noreply.github.com>
2021-07-24 19:09:50 +0900

    edit/updateメソッドを追加

--- application/controllers/User/DailyReport.php
+++ application/controllers/User/DailyReport.php
@@ -7,21 +7,18 @@ class DailyReport extends CI_Controller {
 				parent::__construct();
 				$this->load->model('daily_report_model');
 				$this->load->helper('url_helper');
+				$this->load->helper('form');
+				$this->load->library('form_validation');
 		}
 
 		public function create()
 		{
-				$this->load->helper('form');
-				$this->load->library('form_validation');
-
 				$this->load->view('common/user');
-				$this->load->view('user/create');
+				$this->load->view('user/daily_report/create');
 		}
 
 		public function store()
 		{
-				$this->load->library('form_validation');
-
 				$this->form_validation->set_rules('reporting_time', '日付', 'required|callback_check_input_date', [
 					'required' => '入力必須の項目です。',
 					'check_input_date' => '今日以前の日付を選択してください。'
@@ -47,21 +44,21 @@ class DailyReport extends CI_Controller {
 		}
 
 		public function check_input_date($date)
-    {
-        $today = date("Y/m/d");
-        if ($today < $date | isset($date)) {
-           return TRUE;
-        }
+		{
+				$today = date("Y/m/d");
+				if ($today < $date | isset($date)) {
+				return TRUE;
+				}
 
-        $this->form_validation->set_message('check_input_date', '今日以前の日付を選択してください。');
-        return FALSE;
-    }
+				$this->form_validation->set_message('check_input_date', '今日以前の日付を選択してください。');
+				return FALSE;
+		}
 		
 		public function show($daily_report_id)
 		{
 				$this->load->helper('date');
 				$this->load->helper('typography');
-				
+
 				$data['daily_report'] = $this->daily_report_model->get_daily_report($daily_report_id);
 
 				if (empty($data))
@@ -71,5 +68,56 @@ class DailyReport extends CI_Controller {
 
 				$this->load->view('common/user');
 				$this->load->view('user/daily_report/show', $data);
+		}
+
+		public function edit($daily_report_id)
+		{
+				$this->load->helper('date');
+				$this->load->helper('typography');
+
+				$data['daily_report'] = $this->daily_report_model->get_daily_report($daily_report_id);
+
+				if (empty($data))
+				{
+						return show_404();
+				}
+
+				$this->load->view('common/user');
+				$this->load->view('user/daily_report/edit', $data);
+		}
+
+		public function update($daily_report_id)
+		{
+				$this->load->helper('date');
+
+				$data['daily_report'] = $this->daily_report_model->get_daily_report($daily_report_id);
+
+				if (empty($data))
+					{
+							return show_404();
+					}
+
+				$this->form_validation->set_rules('reporting_time', '日付', 'required|callback_check_input_date', [
+					'required' => '入力必須の項目です。',
+					'check_input_date' => '今日以前の日付を選択してください。'
+				]);
+				$this->form_validation->set_rules('title', '日付', 'required|max_length[255]', [
+					'required' => '入力必須の項目です。',
+					'max_length' => '{param}文字以内で入力してください。'
+				]);
+				$this->form_validation->set_rules('content', '日付', 'required|max_length[1000]', [
+					'required' => '入力必須の項目です。',
+					'max_length' => '{param}文字以内で入力してください。'
+				]);
+
+				if ($this->form_validation->run() === FALSE) {
+					$this->load->view('common/user');
+					$this->load->view('user/daily_report/edit', $data);
+				} else {
+					$this->daily_report_model->update_entry($daily_report_id);
+					return 'success';
+					// $this->load->view('common/user');
+					// $this->load->view('user/index');
+				}
 		}
 }
